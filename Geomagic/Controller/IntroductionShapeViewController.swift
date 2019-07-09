ec4dac9ab86bdb49a4213572a2ac288abe521c0d
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-09 16:22:51 +0700

    Init

---
+++ Geomagic/Controller/IntroductionShapeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IntroductionShapeViewController.swift
+//  Geomagic
+//
+//  Created by Vebby Clarissa on 09/07/19.
+//  Copyright © 2019 Geomecin. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionShapeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
