29f484a9b36da410b1cd5ad1a9015b7d771c7e8f
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-12 03:38:19 +0700

    Create page menu, add classes: RoundButton ; Shape ; Custom cell menu Add checker for initial launch app

--- Geomagic/Controller/HomeViewController.swift
+++ Geomagic/Controller/HomeViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
 
+    var backgroundMusic : AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
+        initBackgroundMusic()
         // Do any additional setup after loading the view.
+    }
+    
+    func initBackgroundMusic(){
+        guard let path = Bundle.main.path(forResource: "backsound", ofType: "mp3") else {return}
+        let url = URL(fileURLWithPath: path)
+        
+        backgroundMusic = try? AVAudioPlayer(contentsOf: url, fileTypeHint: nil)
+        
+        backgroundMusic?.prepareToPlay()
+        
+        backgroundMusic?.setVolume(0.2, fadeDuration: 0.1)
+        
+        backgroundMusic?.play()
+        
     }
 
 
