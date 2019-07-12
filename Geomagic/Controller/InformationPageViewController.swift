2813652d6b085b475f8909b87fd89e343465c112
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-13 02:30:42 +0700

    add layout view

--- Geomagic/Controller/InformationPageViewController.swift
+++ Geomagic/Controller/InformationPageViewController.swift
@@ -9,13 +9,39 @@
 import UIKit
 
 class InformationPageViewController: UIViewController {
-
+    @IBOutlet var baseTitleView: UIView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var baseInformationTextField: UIView!
+    @IBOutlet var informationTextField: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        constructView()
+        writeTitle(title: "This is the title")
+        writeInformationText(longText: "adhfbk asdbhfahbd acbdkjhfasdhf basdkhbfalshdbflaksdjbasdhfba \n- dajdfhaksdjfa sdbfa \n- kadsjhfka hdsbfahd")
         // Do any additional setup after loading the view.
     }
     
+    func constructView () {
+        //create round corner
+        baseTitleView.layer.cornerRadius = 30
+        
+        //create shadow
+        baseTitleView.layer.shadowOpacity = 0.5
+        baseTitleView.layer.shadowRadius = 4
+        
+        let roundedRect = CGRect(x: 5, y: 5, width: baseTitleView.frame.width*0.82, height: baseTitleView.frame.height)
+        print (roundedRect, 370 ,  baseTitleView.frame.height)
+        baseTitleView.layer.shadowPath = UIBezierPath(roundedRect: roundedRect, cornerRadius: baseTitleView.layer.cornerRadius).cgPath
+    }
+    
+    func writeTitle(title:String) {
+        titleLabel.text = title
+    }
+    
+    func writeInformationText (longText : String) {
+        informationTextField.text = longText
+    }
 
     /*
     // MARK: - Navigation
