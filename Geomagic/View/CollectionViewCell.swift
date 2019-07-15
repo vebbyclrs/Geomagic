d3f6f39b355bb2d63d6f7d8302452b94985bdf17
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-15 20:20:59 +0700

    add button action

--- Geomagic/View/CollectionViewCell.swift
+++ Geomagic/View/CollectionViewCell.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+protocol CollectionViewCellDelegate {
+    func pressPengenalanButton(sender:UIButton,cell:UICollectionViewCell)
+    func pressKelilingButton(sender:UIButton,cell:UICollectionViewCell)
+    func pressLuasButton(sender:UIButton,cell:UICollectionViewCell)
+    func pressLatihanButton(sender:UIButton,cell:UICollectionViewCell)
+    
+}
+
 class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var baseView: UIView!
     @IBOutlet weak var shapeImage: UIImageView!
@@ -21,31 +29,28 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet var luasButton: RoundButton!
     @IBOutlet var latihanButton: RoundButton!
     
+    var collectionViewDelegate:CollectionViewCellDelegate?
+    
+    func initClickButton() {
+        pengenalanButton.addTarget(self, action: #selector(self.pressPengenalanButton(_:)), for: .touchUpInside)
+        kelilingButton.addTarget(self, action: #selector(self.pressKelilingButton(_:)), for: .touchUpInside)
+        luasButton.addTarget(self, action: #selector(self.pressLuasButton(_:)), for: .touchUpInside)
+        latihanButton.addTarget(self, action: #selector(self.pressLatihanButton(_:)), for: .touchUpInside)
+    }
+    
+    //The target function
+    @objc func pressPengenalanButton(_ sender: UIButton){ //<- needs `@objc`
+        self.collectionViewDelegate?.pressPengenalanButton(sender:sender, cell: self)
+    }
+    @objc func pressKelilingButton(_ sender: UIButton){ //<- needs `@objc`
+        self.collectionViewDelegate?.pressKelilingButton(sender:sender, cell: self)
+    }
+    @objc func pressLuasButton(_ sender: UIButton){ //<- needs `@objc`
+       self.collectionViewDelegate?.pressLuasButton(sender:sender, cell: self)
+    }
+    @objc func pressLatihanButton(_ sender: UIButton){ //<- needs `@objc`
+        self.collectionViewDelegate?.pressLatihanButton(sender:sender, cell: self)
+    }
+    
 }
 
-extension UIColor {
-    public convenience init?(hex: String, alpha: CGFloat) {
-        let r, g, b: CGFloat
-        
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: alpha)
-                    return
-                }
-            }
-        }
-        
-        return nil
-    }
-}
