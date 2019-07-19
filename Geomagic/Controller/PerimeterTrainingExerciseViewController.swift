0d62834ae4f632ef352ad3c804350b45346a6e5b
Dany Hakim <dany_hakim@yahoo.com>
2019-07-19 21:16:45 +0700

    Add Perimeter Training Intro and Perimeter Training  (with View Controller)

---
+++ Geomagic/Controller/PerimeterTrainingExerciseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PerimeterTrainingExerciseViewController.swift
+//  Geomagic
+//
+//  Created by Dany Hakim on 19/07/19.
+//  Copyright © 2019 Geomecin. All rights reserved.
+//
+
+import UIKit
+
+class PerimeterTrainingExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+
+}
---
+++ Geomagic/Controller/PerimeterTrainingIntroViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PerimeterTrainingIntroViewController.swift
+//  Geomagic
+//
+//  Created by Dany Hakim on 19/07/19.
+//  Copyright © 2019 Geomecin. All rights reserved.
+//
+
+import UIKit
+
+class PerimeterTrainingIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
