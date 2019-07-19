9dc69fcb16fecf6e1a35194a9e2f6aace2e695e6
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-19 18:02:58 +0700

    nyobain si template

---
+++ Geomagic/Controller/SubMenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SubMenuViewController.swift
+//  Geomagic
+//
+//  Created by Vebby Clarissa on 19/07/19.
+//  Copyright © 2019 Geomecin. All rights reserved.
+//
+
+import UIKit
+
+class SubMenuViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func prepareView() {
+//        titleLabel.frame. = self.view.frame.width
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
