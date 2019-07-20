b66abd08201b362f99479a684d30aeea223ed887
Dany Hakim <dany_hakim@yahoo.com>
2019-07-20 23:55:03 +0700

    Complete Perimeter Training Exercise

--- Geomagic/Controller/PerimeterTrainingExerciseViewController.swift
+++ Geomagic/Controller/PerimeterTrainingExerciseViewController.swift
@@ -32,6 +32,9 @@ class PerimeterTrainingExerciseViewController: UIViewController {
     var label9downOrigin:CGPoint!
     var label5leftOrigin:CGPoint!
     var label5rightOrigin:CGPoint!
+    var option26Origin:CGPoint!
+    var option27Origin:CGPoint!
+    var option28Origin:CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,13 +52,20 @@ class PerimeterTrainingExerciseViewController: UIViewController {
         view.bringSubviewToFront(box2label)
         view.bringSubviewToFront(box3label)
         view.bringSubviewToFront(box4label)
+        
+            view.bringSubviewToFront(option28)
         label9upOrigin = label9up.frame.origin
         label9downOrigin = label9down.frame.origin
         label5leftOrigin = label5left.frame.origin
         label5rightOrigin = label5right.frame.origin
+        option26Origin = option26.frame.origin
+        option27Origin = option27.frame.origin
+        option28Origin = option28.frame.origin
 
     }
  
+    // MARK: Pan Gesture For Label and Option
+    
     @IBAction func pan9up(_ sender: UIPanGestureRecognizer) {
         //ambil untuk translasi
             guard let recognizerLabel = sender.view else {
@@ -106,29 +116,7 @@ class PerimeterTrainingExerciseViewController: UIViewController {
                 }
                 
             }
-            
-            
-            
-//            if label4.frame.intersects(box1.frame) {
-//                UILabel.animate(withDuration: 0.3) {
-//                    self.label4.alpha = 0.0
-//                }
-//                boxLabel1.text = label4.text
-//                boxLabel1.isHidden = false
-//            }else if label4.frame.intersects(box2.frame){
-//                UILabel.animate(withDuration: 0.3) {
-//                    self.label4.alpha = 0.0
-//                }
-//                boxLabel2.text = label4.text
-//                boxLabel2.isHidden = false
-//            }
-//            else{
-//                //kalo ga masuk ke boxnya balik lagi ke tempat semula
-//                UILabel.animate(withDuration: 0.3) {
-//                    self.label4.frame.origin = self.label4Origin
-//                }
-//            }
-//            showOption()
+            showOption()
           default:
             break
         }
@@ -136,12 +124,265 @@ class PerimeterTrainingExerciseViewController: UIViewController {
     }
     
     @IBAction func pan9down(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            //kalo bersinggungan dengan box1 s/d box4
+            if label9down.frame.intersects(box1.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label9down.alpha = 0.0
+                }
+                box1label.text = label9down.text
+                box1label.isHidden = false
+            }
+            else if label9down.frame.intersects(box2.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label9down.alpha = 0.0
+                }
+                box2label.text = label9down.text
+                box2label.isHidden = false
+            }
+            else if label9down.frame.intersects(box3.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label9down.alpha = 0.0
+                }
+                box3label.text = label9down.text
+                box3label.isHidden = false
+            }
+            else if label9down.frame.intersects(box4.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label9down.alpha = 0.0
+                }
+                box4label.text = label9down.text
+                box4label.isHidden = false
+            }
+            else{
+                //kalo ga ke salah satu kotak,balik ke awal
+                UILabel.animate(withDuration: 0.3) {
+                    self.label9down.frame.origin = self.label9downOrigin
+                }
+                
+            }
+            showOption()
+        default:
+            break
+        }
     }
     
     @IBAction func pan5right(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            //kalo bersinggungan dengan box1 s/d box4
+            if label5right.frame.intersects(box1.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5right.alpha = 0.0
+                }
+                box1label.text = label5right.text
+                box1label.isHidden = false
+            }
+            else if label5right.frame.intersects(box2.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5right.alpha = 0.0
+                }
+                box2label.text = label5right.text
+                box2label.isHidden = false
+            }
+            else if label5right.frame.intersects(box3.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5right.alpha = 0.0
+                }
+                box3label.text = label5right.text
+                box3label.isHidden = false
+            }
+            else if label5right.frame.intersects(box4.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5right.alpha = 0.0
+                }
+                box4label.text = label5right.text
+                box4label.isHidden = false
+            }
+            else{
+                //kalo ga ke salah satu kotak,balik ke awal
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5right.frame.origin = self.label5rightOrigin
+                }
+                
+            }
+            showOption()
+        default:
+            break
+        }
     }
     
     @IBAction func pan5left(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            //kalo bersinggungan dengan box1 s/d box4
+            if label5left.frame.intersects(box1.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5left.alpha = 0.0
+                }
+                box1label.text = label5left.text
+                box1label.isHidden = false
+            }
+            else if label5left.frame.intersects(box2.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5left.alpha = 0.0
+                }
+                box2label.text = label5left.text
+                box2label.isHidden = false
+            }
+            else if label5left.frame.intersects(box3.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5left.alpha = 0.0
+                }
+                box3label.text = label5left.text
+                box3label.isHidden = false
+            }
+            else if label5left.frame.intersects(box4.frame){
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5left.alpha = 0.0
+                }
+                box4label.text = label5left.text
+                box4label.isHidden = false
+            }
+            else{
+                //kalo ga ke salah satu kotak,balik ke awal
+                UILabel.animate(withDuration: 0.3) {
+                    self.label5left.frame.origin = self.label5leftOrigin
+                }
+                
+            }
+            showOption()
+        default:
+            break
+        }
     }
     
+    @IBAction func pan26option(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            if option26.frame.intersects(boxResult.frame){
+                boxResult.shakeResult()
+                self.option26.frame.origin = self.option26Origin
+            }else{
+                UIView.animate(withDuration: 0.3) {
+                    self.option26.frame.origin = self.option26Origin
+                }
+            }
+        default:
+            break
+        }
+        
+    }
+    
+    @IBAction func pan27option(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            if option27.frame.intersects(boxResult.frame){
+                boxResult.shakeResult()
+                self.option27.frame.origin = self.option27Origin
+            }else{
+                UIView.animate(withDuration: 0.3) {
+                    self.option27.frame.origin = self.option27Origin
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    @IBAction func pan28option(_ sender: UIPanGestureRecognizer) {
+        guard let recognizerLabel = sender.view else {
+            return
+        }
+        
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            recognizerLabel.center.x += translation.x
+            recognizerLabel.center.y += translation.y
+            sender.setTranslation(.zero, in: view)
+        case .ended:
+            if option28.frame.intersects(boxResult.frame){
+                UIView.animate(withDuration: 0.3) {
+                    self.option28.alpha = 0.0
+                    self.label28.alpha = 0.0
+                }
+                boxResultLabel.text = label28.text
+                boxResultLabel.isHidden = false
+            }else{
+                UIView.animate(withDuration: 0.3) {
+                    self.option28.frame.origin = self.option28Origin
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+// MARK: Show Option When Box is All Filled
+    func showOption(){
+        if box1label.text != "0" && box2label.text != "0" && box3label.text != "0" && box4label.text != "0" {
+            option26.isHidden = false
+            option27.isHidden = false
+            option28.isHidden = false
+        }
+        
+
+        
+    }
+    
+    
 }
+
