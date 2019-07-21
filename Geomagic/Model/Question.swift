133c397f1daf7d1b16dda4a189f72337e23c6446
anandaayupermata <anandayuperm@gmail.com>
2019-07-21 18:14:59 +0700

    kuis rectangle

--- Geomagic/Model/Question.swift
+++ Geomagic/Model/Question.swift
@@ -10,14 +10,16 @@ import Foundation
 
 class Question {
     
-    let lengthInt: Int
+    let lengthInt1: Int
+    let lengthInt2 : Int
     let answer: String
     let choice1: String
     let choice2: String
     let choice3: String
     
-    init(num: Int, correctAnswer: String, pil1: String, pil2: String, pil3: String) {
-        lengthInt = num
+    init(num1: Int, num2: Int, correctAnswer: String, pil1: String, pil2: String, pil3: String) {
+        lengthInt1 = num1
+        lengthInt2 = num2
         answer = correctAnswer
         choice1 = pil1
         choice2 = pil2
--- Geomagic/Model/SquareQuestion.swift
+++ Geomagic/Model/SquareQuestion.swift
@@ -14,16 +14,16 @@ class SquareQuestion {
     
     init() {
         // Creating a quiz item and appending it to the list
-        list.append(Question(num: 4, correctAnswer: "a", pil1: "16", pil2: "18", pil3: "12"))
-        list.append(Question(num: 4, correctAnswer: "c", pil1: "14", pil2: "15", pil3: "16"))
-        list.append(Question(num: 7, correctAnswer: "c", pil1: "26", pil2: "27", pil3: "28"))
-        list.append(Question(num: 7, correctAnswer: "b", pil1: "28", pil2: "49", pil3: "70"))
-        list.append(Question(num: 9, correctAnswer: "a", pil1: "36", pil2: "27", pil3: "18"))
-        list.append(Question(num: 9, correctAnswer: "c", pil1: "18", pil2: "36", pil3: "81"))
-        list.append(Question(num: 10, correctAnswer: "a", pil1: "40", pil2: "50", pil3: "100"))
-        list.append(Question(num: 10, correctAnswer: "c", pil1: "40", pil2: "70", pil3: "100"))
-        list.append(Question(num: 11, correctAnswer: "c", pil1: "11", pil2: "22", pil3: "44"))
-        list.append(Question(num: 11, correctAnswer: "b", pil1: "44", pil2: "121", pil3: "144"))
+        list.append(Question(num1: 7, num2: 4, correctAnswer: "a", pil1: "22", pil2: "24", pil3: "28"))
+        list.append(Question(num1: 7, num2: 4,  correctAnswer: "c", pil1: "24", pil2: "26", pil3: "28"))
+        list.append(Question(num1: 9, num2: 3,  correctAnswer: "c", pil1: "18", pil2: "20", pil3: "24"))
+        list.append(Question(num1: 9, num2: 3,  correctAnswer: "b", pil1: "18", pil2: "27", pil3: "36"))
+        //        list.append(Question(num: 9, correctAnswer: "a", pil1: "36", pil2: "27", pil3: "18"))
+        //        list.append(Question(num: 9, correctAnswer: "c", pil1: "18", pil2: "36", pil3: "81"))
+        //        list.append(Question(num: 10, correctAnswer: "a", pil1: "40", pil2: "50", pil3: "100"))
+        //        list.append(Question(num: 10, correctAnswer: "c", pil1: "40", pil2: "70", pil3: "100"))
+        //        list.append(Question(num: 11, correctAnswer: "c", pil1: "11", pil2: "22", pil3: "44"))
+        //        list.append(Question(num: 11, correctAnswer: "b", pil1: "44", pil2: "121", pil3: "144"))
         
     }
 }
